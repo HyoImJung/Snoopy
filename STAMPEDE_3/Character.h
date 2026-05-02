288fd1acb110677ed78d17e2a3d1567c18930e22
SeungJun221 <leesj5279@gmail.com>
2026-05-02 19:11:01 +0900

    Add files via upload

---
+++ STAMPEDE_3/Character.h
@@ -0,0 +1,39 @@
+﻿#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+
+enum class CharacterClass { KNIGHT, PRIEST, ARCHER, MAGE, ENEMY_ORC };
+
+struct Character {
+    std::string name;
+    CharacterClass cls;
+    int x, y;
+    int hp, maxHp;
+    int atk;
+    int moveRange;
+    int atkRange;
+    bool isAlly;
+    bool actedThisTurn;
+
+    Character(std::string n, CharacterClass c, int px, int py,
+              int h, int a, int mv, int ar, bool ally)
+        : name(n), cls(c), x(px), y(py),
+          hp(h), maxHp(h), atk(a), moveRange(mv), atkRange(ar),
+          isAlly(ally), actedThisTurn(false) {}
+
+    bool isAlive() const { return hp > 0; }
+
+    std::string getIcon() const {
+        switch (cls) {
+        case CharacterClass::KNIGHT:    return "K";
+        case CharacterClass::PRIEST:    return "P";
+        case CharacterClass::ARCHER:    return "A";
+        case CharacterClass::MAGE:      return "W";
+        case CharacterClass::ENEMY_ORC: return "E";
+        default: return "?";
+        }
+    }
+};
+
+#endif
