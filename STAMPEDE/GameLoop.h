02b874c0e00b49e519e3a3addffc93b41e1c93c0
SeungJun221 <leesj5279@gmail.com>
2026-06-05 14:02:15 +0900

    Delete STAMPEDE directory

--- STAMPEDE/GameLoop.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameMap.h"
-#include "GameState.h"
-
-// Start new game (slot used for saving)
-void runNewGame(int saveSlot);
-
-// Load saved game
-void runLoadGame(int saveSlot);
